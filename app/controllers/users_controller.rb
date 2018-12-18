894435dfa8df7a8601184a177c64ea5732720434
nami.n <nekossss0804@gmail.com>
2018-12-18 05:12:48 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
-   def index
-    @users = User.all.page(params[:page])
-   end
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
-   def show
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
-   end
+  end
 
-   def new
+  def new
     @user = User.new
-   end
+  end
 
-   def create
+  def create
     @user = User.new(user_params)
-    
+
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
@@ -25,7 +25,19 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
-   end
+  end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
 
   private
 
